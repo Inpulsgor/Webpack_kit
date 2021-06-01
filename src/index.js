081c18398d79f4a54d051d73db9fda211f68c73b
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-06-01 08:49:31 +0300

    web-p config

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export const add = (a, b) => a + b;
+
+add(2, 3);
+add(2, 3);
+add(2, 3);
+add(2, 3);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+};
